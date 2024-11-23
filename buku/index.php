b2d574f0f6569fe0ee76083dd9506ba772924104
putrichantikaa <putrichantiikaa@gmail.com>
2024-11-23 09:54:00 +0700

    membuat file form,edit,hapus,update,index,proses,koneksi dan navbar

---
+++ buku/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Buku</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+  <?php 
+    include_once '../navbar.php'; 
+  ?>
+
+<div class="container">
+   <div class="row mt-5">
+        <div class="col-8 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3 class="float-start">Data Buku</h3>
+                <span class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i> Tambah Data </a></span>
+            </div>
+                <div class="card-body">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">ID Buku</th>
+      <th scope="col">Judul Buku</th>
+      <th scope="col">Pengarang</th>
+      <th scope="col">Tahun Terbit</th>
+      <th scope="col">Kategori</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    #1. koneksikan file ini
+    include("../koneksi.php");
+
+    #2. menulis query
+    $tampil = "SELECT * FROM buku";
+
+    #3. jalankan query
+    $proses = mysqli_query($koneksi,$tampil);
+
+    #$4. looping data dari database
+    $nomor = 1;
+    foreach($proses as $data){
+    ?>
+    <tr>
+      <th scope="row"><?= $nomor++ ?></th>
+      <td><?= $data['id_buku'] ?></td>
+      <td><?= $data['judul_buku'] ?></td>
+      <td><?= $data['pengarang'] ?></td>
+      <td><?= $data['tahun_terbit'] ?></td>
+      <td><?= $data['kategori'] ?></td>
+      <td>
+        <a class="btn btn-info btn-sm" href="edit.php?id=<?=$data['id_buku']?>"><i class="fa-solid fa-pen-to-square"></i></a>
+
+        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?=$data['id_buku']?>">
+        <i class="fa-solid fa-trash"></i>
+        </button>
+
+        <!-- Modal -->
+<div class="modal fade" id="hapus<?=$data['id_buku']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      Yakin data <b><?=$data['buku']?></b> ingin dihapus?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+        <a href="hapus.php?xyz=<?=$data['id_buku']?>" class="btn btn-danger">Hapus</a>
+      </div>
+    </div>
+  </div>
+</div>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+  </tbody>
+</table>
+            </div>
+        </div>
+   </div>
+</div>
+
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+  <?php 
+    include_once 'navbar.php'; 
+  ?>
+
+<div class="container">
+   <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header text-center">
+                <h4>Daftar Buku</h4>
+            </div>
+            <div class="card-body">
+              <h4>Selamat Datang Di Library Manager</h4>
+              <p>Tuliskan Kata-kata hari ini </p>
+            </div>
+            </div>
+        </div>
+   </div>
+</div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+
+</body>
+</html>
